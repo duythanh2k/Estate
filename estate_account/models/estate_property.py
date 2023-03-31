37fbabc0dbb11bf18282c843d21f5b99492e664c
Phạm Duy Thành <53567335+duythanh2k@users.noreply.github.com>
2023-03-31 10:07:02 +0700

    Estate Account

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -4,31 +4,41 @@ from odoo import models, fields, api, Command
 
 
 class EstateProperty(models.Model):
-    _inherit = "estate.property"
+    _inherit = 'estate.property'
 
-#    @Override method
+#   @Override method
     def action_sold(self):
-        invoice_vals = {}
-        # Add invoice_line_ids
-        line_ids = [
-            Command.create({
-                "name": "6% of the selling price",
-                "quantity": 5,
-                "price_unit": self.selling_price * 6/100,
-            }),
-            Command.create({
-                "name": "An additional 100.00 from administrative fees",
-                "quantity": 5,
-                "price_unit": 100.00,
-            }),
-        ]
-        # Invoice values
-        invoice_vals['partner_id'] = self.buyer
-        invoice_vals['move_type'] = "out_invoice"
-        invoice_vals['journal_id'] = self.env['account.move'].with_context(default_move_type='out_invoice')._get_default_journal().id
-        invoice_vals['invoice_line_ids'] = line_ids
-        # Create an empty account.move object
-        self.env['account.move'].create(invoice_vals)
-        # print('FUCK: {}'.format(self.env['account.move'].with_context(default_move_type='out_invoice')._get_default_journal().id))
-        
-        return super().action_sold()
+        res = super().action_sold()
+        journal = self.env["account.journal"].search([("type", "=", "sale")], limit=1)
+        # Another way to get the journal:
+        # journal = self.env["account.move"].with_context(default_move_type="out_invoice")._get_default_journal()
+        for record in self:
+            self.env["account.move"].create(
+                {
+                    "partner_id": record.buyer.id,
+                    "move_type": "out_invoice",
+                    "journal_id": journal.id,
+                    "invoice_line_ids": [
+                        (
+                            0,
+                            0,
+                            {
+                                "name": record.name,
+                                "quantity": 1.0,
+                                "price_unit": record.selling_price * 6.0 / 100.0,
+                            },
+                        ),
+                        (
+                            0,
+                            0,
+                            {
+                                "name": "Administrative fees",
+                                "quantity": 1.0,
+                                "price_unit": 100.0,
+                            },
+                        ),
+                    ],
+                }
+            )
+
+        return res
